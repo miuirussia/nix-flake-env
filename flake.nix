9316d59db478b594258f328cf5c2ee1488ca839f
Kirill Kuznetsov <kirill.desirehd@gmail.com>
2021-09-03 17:38:30 +0300

    Add darwin and home-manager

--- flake.nix
+++ flake.nix
@@ -4,11 +4,15 @@
   inputs = {
     nixpkgs.url = "github:miuirussia/nixpkgs";
 
+    darwin.url = "github:LnL7/nix-darwin/master";
+    darwin.inputs.nixpkgs.follows = "nixpkgs";
+
+    home-manager.url = "github:miuirussia/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+
     flake-utils.url = "github:numtide/flake-utils";
-    flake-compat = {
-      url = "github:edolstra/flake-compat";
-      flake = false;
-    };
+    flake-compat.url = "github:edolstra/flake-compat";
+    flake-compat.flake = false;
   };
 
   outputs = inputs @ { self, nixpkgs, flake-utils, ... }:
