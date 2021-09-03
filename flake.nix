7d5e346c896d86457eed31b476b7be637d61a48a
Kirill Kuznetsov <kirill.desirehd@gmail.com>
2021-09-03 15:14:57 +0300

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:miuirussia/nixpkgs";
+
+    flake-utils.url = "github:numtide/flake-utils";
+    flake-compat = {
+      url = "github:edolstra/flake-compat";
+      flake = false;
+    };
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+          {
+            packages.hello = pkgs.hello;
+
+            defaultPackage = pkgs.hello;
+          }
+    );
+}
