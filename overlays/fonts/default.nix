fcb0f4077a35f4fbc2161d20b6faf81333a8adbb
Kirill Kuznetsov <kirill.desirehd@gmail.com>
2021-09-04 23:04:26 +0300

    Fix jetbrains-mono version

--- overlays/fonts/default.nix
+++ overlays/fonts/default.nix
@@ -42,33 +42,31 @@ in
 {
   inherit font-patcher;
 
-  jetbrains-mono = let
-    version = "master";
-  in
-    prev.stdenv.mkDerivation {
-      name = "jetbrains-mono-${version}";
+  jetbrains-mono = prev.stdenv.mkDerivation {
+    name = "jetbrains-mono";
+    version = inputs.jetbrains-mono.shortRev;
 
-      src = inputs.jetbrains-mono;
+    src = inputs.jetbrains-mono;
 
-      buildPhase = ''
-        export PYTHONIOENCODING=utf8
-        for filename in ./fonts/ttf/*.ttf; do
-          ${final.fontforge}/bin/fontforge -script ${font-patcher}/bin/font-patcher $filename -c -out ./out
-        done
-      '';
+    buildPhase = ''
+      export PYTHONIOENCODING=utf8
+      for filename in ./fonts/ttf/*.ttf; do
+        ${final.fontforge}/bin/fontforge -script ${font-patcher}/bin/font-patcher $filename -c -out ./out
+      done
+    '';
 
-      installPhase = ''
-        mkdir -p $out/share/fonts $out/share/fonts/truetype $out/share/fonts/woff2
-        cp ./out/*.ttf -d $out/share/fonts/truetype
-        cp ./fonts/webfonts/*.woff2 -d $out/share/fonts/woff2
-      '';
+    installPhase = ''
+      mkdir -p $out/share/fonts $out/share/fonts/truetype $out/share/fonts/woff2
+      cp ./out/*.ttf -d $out/share/fonts/truetype
+      cp ./fonts/webfonts/*.woff2 -d $out/share/fonts/woff2
+    '';
 
-      meta = with final.lib; {
-        description = "A typeface made for developers";
-        homepage = "https://jetbrains.com/mono/";
-        license = licenses.asl20;
-        maintainers = [ maintainers.marsam ];
-        platforms = platforms.all;
-      };
+    meta = with final.lib; {
+      description = "A typeface made for developers";
+      homepage = "https://jetbrains.com/mono/";
+      license = licenses.asl20;
+      maintainers = [ maintainers.marsam ];
+      platforms = platforms.all;
     };
+  };
 }
