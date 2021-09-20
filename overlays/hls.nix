91865599a7b96b9c27ae98fdbf5cd202c5d70e40
Kirill Kuznetsov <kirill.desirehd@gmail.com>
2021-09-20 20:56:14 +0300

    Add build-hls

--- overlays/hls.nix
+++ overlays/hls.nix
@@ -1,23 +1,28 @@
 inputs: final: prev: let
   mkHlsPackage = ghcVersion: inputs.hls-nix.lib."${final.system}"."${ghcVersion}";
 
-  hls865 = mkHlsPackage "ghc865";
-  hls884 = mkHlsPackage "ghc884";
-  hls8106 = mkHlsPackage "ghc8106";
-  hls8107 = mkHlsPackage "ghc8107";
+  hls865package = mkHlsPackage "ghc865";
+  hls884package = mkHlsPackage "ghc884";
+  hls8106package = mkHlsPackage "ghc8106";
+  hls8107package = mkHlsPackage "ghc8107";
 in
 {
   hls = prev.buildEnv {
     name = "haskell-language-server";
 
     paths = [
-      hls865.hls-renamed
-      hls884.hls-renamed
-      hls8106.hls-renamed
-      hls8107.hls-renamed
-      hls8107.hls-wrapper
+      hls865package.hls-renamed
+      hls884package.hls-renamed
+      hls8106package.hls-renamed
+      hls8107package.hls-renamed
+      hls8107package.hls-wrapper
     ];
   };
+
+  hls865 = hls865package.hls;
+  hls884 = hls884package.hls;
+  hls8106 = hls8106package.hls;
+  hls8107 = hls8107package.hls;
 
   haskell = let
     mkGhcPackage = ghcVersion: inputs.hls-nix.lib."${final.system}"."${ghcVersion}".ghc;
