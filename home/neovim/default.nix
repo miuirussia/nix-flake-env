65f52243eb50dc460032dbeea920cea4fb90875a
Kirill Kuznetsov <kirill.desirehd@gmail.com>
2021-12-26 20:57:49 +0300

    Remove rust overlay

--- home/neovim/default.nix
+++ home/neovim/default.nix
@@ -11,7 +11,7 @@
       prettier = pkgs.nodePackages.prettier;
       stylua = pkgs.stylua;
       diagnosticls = pkgs.nodePackages.diagnostic-languageserver;
-      rust_analyzer = pkgs.rust-analyzer-nightly-with-shell;
+      rust_analyzer = pkgs.rust-analyzer-nightly;
     };
 
     nvim-kdevlab-luaconfig = pkgs.vimUtils.buildVimPluginFrom2Nix {
