509c8e850e91ab390c658d552b0c2d60c90abc9b
Kirill Kuznetsov <kirill.desirehd@gmail.com>
2022-01-08 16:26:36 +0300

    Don't do install check

--- overlays/nixUnstable/default.nix
+++ overlays/nixUnstable/default.nix
@@ -1,5 +1,6 @@
 inputs: final: prev: {
   nixUnstable = final.lowPrio (inputs.nixUnstable.defaultPackage.${final.system}.overrideAttrs (_: {
     doCheck = false;
+    doInstallCheck = false;
   }));
 }
