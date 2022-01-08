51dd8009f01a3399eb6c92b9be2445f65db5503a
Kirill Kuznetsov <kirill.desirehd@gmail.com>
2022-01-08 23:59:01 +0300

    Add hls902 and hls921

--- overlays/hls.nix
+++ overlays/hls.nix
@@ -3,13 +3,19 @@ inputs: final: prev: let
 
   hls884package = mkHlsPackage "ghc884";
   hls8107package = mkHlsPackage "ghc8107";
+  hls902package = mkHlsPackage "ghc902";
+  hls921package = mkHlsPackage "ghc921";
 in
 {
-  hls = hls8107package.mkHlsWrapper [
+  hls = hls921package.mkHlsWrapper [
     hls884package.hls-renamed
     hls8107package.hls-renamed
+    hls902package.hls-renamed
+    hls921package.hls-renamed
   ];
 
   hls884 = hls884package.hls;
   hls8107 = hls8107package.hls;
+  hls902 = hls902package.hls;
+  hls921 = hls921package.hls;
 }
