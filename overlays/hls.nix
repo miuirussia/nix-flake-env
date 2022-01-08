c63423ce318d56e574f6846077a83d9c1e7824ba
Kirill Kuznetsov <kirill.desirehd@gmail.com>
2022-01-08 19:33:54 +0300

    Bump hls-nix

--- overlays/hls.nix
+++ overlays/hls.nix
@@ -1,30 +1,21 @@
 inputs: final: prev: let
   mkHlsPackage = ghcVersion: inputs.hls-nix.lib."${final.system}"."${ghcVersion}";
 
-  hls865package = mkHlsPackage "ghc865";
   hls884package = mkHlsPackage "ghc884";
-  hls8106package = mkHlsPackage "ghc8106";
   hls8107package = mkHlsPackage "ghc8107";
+  hls901package = mkHlsPackage "ghc901";
+  hls921package = mkHlsPackage "ghc921";
 in
 {
-  hls = hls8107package.mkHlsWrapper [
-    hls865package.hls-renamed
+  hls = hls921package.mkHlsWrapper [
     hls884package.hls-renamed
-    hls8106package.hls-renamed
     hls8107package.hls-renamed
+    hls901package.hls-renamed
+    hls921package.hls-renamed
   ];
 
-  hls865 = hls865package.hls;
   hls884 = hls884package.hls;
-  hls8106 = hls8106package.hls;
   hls8107 = hls8107package.hls;
-
-  haskell = let
-    mkGhcPackage = ghcVersion: inputs.hls-nix.lib."${final.system}"."${ghcVersion}".ghc;
-  in
-    prev.haskell // {
-      compiler = prev.haskell.compiler // {
-        ghc865 = mkGhcPackage "ghc865";
-      };
-    };
+  hls901 = hls901package.hls;
+  hls921 = hls921package.hls;
 }
