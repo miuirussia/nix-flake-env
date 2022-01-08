a5705ade7159e440f127cf61a6eaed0eefea252f
Kirill Kuznetsov <kirill.desirehd@gmail.com>
2022-01-08 21:49:32 +0300

    Disable hls for ghc 9.x.x

--- overlays/hls.nix
+++ overlays/hls.nix
@@ -3,16 +3,13 @@ inputs: final: prev: let
 
   hls884package = mkHlsPackage "ghc884";
   hls8107package = mkHlsPackage "ghc8107";
-  hls921package = mkHlsPackage "ghc921";
 in
 {
-  hls = hls921package.mkHlsWrapper [
+  hls = hls8107package.mkHlsWrapper [
     hls884package.hls-renamed
     hls8107package.hls-renamed
-    hls921package.hls-renamed
   ];
 
   hls884 = hls884package.hls;
   hls8107 = hls8107package.hls;
-  hls921 = hls921package.hls;
 }
