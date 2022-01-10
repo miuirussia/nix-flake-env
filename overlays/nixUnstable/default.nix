a7379885278287a8696f4d8ba245cba1d55ce2e5
Kirill Kuznetsov <kirill.desirehd@gmail.com>
2022-01-10 14:22:16 +0300

    Temporary remove nixUnstable

--- overlays/nixUnstable/default.nix
+++ overlays/nixUnstable/default.nix
@@ -1,6 +1,6 @@
 inputs: final: prev: {
-  nixUnstable = final.lowPrio (inputs.nixUnstable.defaultPackage.${final.system}.overrideAttrs (_: {
-    doCheck = false;
-    doInstallCheck = false;
-  }));
+  # nixUnstable = final.lowPrio (inputs.nixUnstable.defaultPackage.${final.system}.overrideAttrs (_: {
+  #  doCheck = false;
+  #  doInstallCheck = false;
+  # }));
 }
