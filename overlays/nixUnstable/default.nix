50d7dfb650470a23554f64548357b81810fb560d
Kirill Kuznetsov <kirill.desirehd@gmail.com>
2022-01-20 09:14:50 +0300

    Build without tests

--- overlays/nixUnstable/default.nix
+++ overlays/nixUnstable/default.nix
@@ -1,3 +1,6 @@
 inputs: final: prev: {
-  nixUnstable = final.lowPrio inputs.nixUnstable.defaultPackage.${final.system};
+  nixUnstable = final.lowPrio (inputs.nixUnstable.defaultPackage.${final.system}.overrideAttrs (_: {
+    doCheck = false;
+    doInstallCheck = false;
+  }));
 }
