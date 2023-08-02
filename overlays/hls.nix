40c775aa57b45fbb9f25cbf9cf764a5e5babdb7d
Kirill Kuznetsov <kkuznetsov@termt.com>
2023-08-02 17:26:45 +0300

    Use new haskell-language-server

---
+++ overlays/hls.nix
@@ -0,0 +1,24 @@
+inputs: final: prev:
+
+let
+  unstable = import inputs.nixpkgs-unstable {
+    system = prev.system;
+    config = {
+      allowUnfree = true;
+    };
+  };
+in
+{
+  hls = (unstable.haskell-language-server.override {
+    supportedGhcVersions = [
+      "8107"
+      "902"
+    ];
+  }).overrideAttrs (oldAttrs: {
+    buildInputs = oldAttrs.buildInputs or [] ++ [ prev.makeWrapper ];
+    postInstall = oldAttrs.postInstall or "" + ''
+      wrapProgram $out/bin/haskell-language-server-wrapper \
+        --prefix PATH : $out/bin
+    ''; 
+  });
+}
