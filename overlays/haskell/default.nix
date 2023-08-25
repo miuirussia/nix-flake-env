301c93e1b59db38de81207dec7973c852f709025
Kirill Kuznetsov <kkuznetsov@termt.com>
2023-08-25 17:26:48 +0300

    Fix build

--- overlays/haskell/default.nix
+++ overlays/haskell/default.nix
@@ -6,10 +6,15 @@ inputs: final: prev: {
         patches = [ ./dhall-lsp-server.patch ];
       });
       ilist = prev.haskell.lib.overrideCabal hsSuper.ilist (_: {
+        revision = "3";
+        sha256 = "sha256-BEiFcpaXQxfuFiVR7z4vMcQ04RTfbRfX9qzTR2xS3AQ=";
+        editedCabalFile = "sha256-EmAZ+vPQLzBcV2KulgK+HL92KRFToadGEnCtFClngRE=";
         broken = false;
       });
       spdx = prev.haskell.lib.overrideCabal hsSuper.spdx (_: {
+        doCheck = false;
         broken = false;
+        patches = [ ./spdx.patch ];
       });
     };
   };
